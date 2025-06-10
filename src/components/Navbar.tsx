5e03c34aa92dfa794401fef0784557feea862cbb
Keana Delmotte <keana.delmotte@gmail.com>
2025-06-10 14:01:58 +0200

    Added navbar with signin/out menu

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,100 @@
+"use client";
+import { User } from "next-auth";
+import { signOut, signIn } from "next-auth/react";
+import Image from "next/image";
+import styles from "./Navbar.module.css";
+import React, { useState, useEffect, useRef } from "react";
+import { FaUser } from "react-icons/fa6";
+
+interface NavbarProps {
+	user: User | undefined;
+}
+export default function Navbar({ user }: NavbarProps) {
+	const [showMenu, setShowMenu] = useState(false);
+	const menuRef = useRef<HTMLDivElement>(null);
+
+	const handleClickOutside = (event: MouseEvent) => {
+		if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+			setShowMenu(false);
+		}
+	};
+
+	useEffect(() => {
+		if (showMenu) {
+			document.addEventListener("mousedown", handleClickOutside);
+		} else {
+			document.removeEventListener("mousedown", handleClickOutside);
+		}
+
+		return () => {
+			document.removeEventListener("mousedown", handleClickOutside);
+		};
+	}, [showMenu]);
+	return (
+		<div className={styles.navbar}>
+			<p className={styles.title}>Recipe Manager</p>
+
+			<div className={styles.options}>
+				{user && (
+					<div className={styles.user}>
+						<Image
+							src={user.image ?? "public/default-user.svg"}
+							alt="user image"
+							className={styles.userImage}
+							width={30}
+							height={30}
+							onClick={() => setShowMenu(true)}
+						/>
+						<p>{`Welcome, ${user.name}`}</p>
+					</div>
+				)}
+				{!user && (
+					<FaUser
+						className={styles.userImage}
+						onClick={() => setShowMenu(true)}
+					/>
+				)}
+				{showMenu && (
+					<UserMenu
+						user={user}
+						ref={menuRef}
+						dismiss={() => setShowMenu(false)}
+					/>
+				)}
+			</div>
+		</div>
+	);
+}
+
+interface UserMenuProps {
+	user: User | undefined;
+	ref: React.RefObject<HTMLDivElement | null>;
+	dismiss: () => void;
+}
+
+function UserMenu({ user, ref, dismiss }: UserMenuProps) {
+	return (
+		<div className={styles.userMenu} ref={ref}>
+			{user && (
+				<button
+					onClick={async () => {
+						await signOut();
+						dismiss();
+					}}
+				>
+					Sign Out
+				</button>
+			)}
+			{!user && (
+				<button
+					onClick={async () => {
+						await signIn();
+						dismiss();
+					}}
+				>
+					Sign In
+				</button>
+			)}
+		</div>
+	);
+}
