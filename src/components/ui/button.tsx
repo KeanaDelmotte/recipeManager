c666c75bb7b3bf09778c58a0f5cfab1cb770ad3e
Keana Delmotte <keana.delmotte@gmail.com>
2025-07-02 12:00:23 +0200

    Added styling to buttons

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,20 +3,27 @@ import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/lib/utils";
-
+const hoverEffect = " hover:shadow-lg hover:scale-103 ";
+const activeEffect = " active:shadow-sm active:scale-96 ";
 const buttonVariants = cva(
 	"inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
 	{
 		variants: {
 			variant: {
 				default:
-					"bg-primary text-primary-foreground shadow-xs hover:bg-primary/90 hover:bg-primary-darker hover:text-secondary-foreground cursor-pointer transition-all duration-200",
+					"bg-primary text-primary-foreground shadow-xs hover:bg-primary/90 hover:bg-primary-darker hover:text-secondary-foreground cursor-pointer transition-all duration-200" +
+					hoverEffect +
+					activeEffect,
 				destructive:
-					"bg-destructive text-white shadow-xs hover:bg-destructive-darker focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60 cursor-pointer transition-all duration-200",
+					"bg-destructive text-white shadow-xs hover:bg-destructive-darker focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60 cursor-pointer transition-all duration-200" +
+					hoverEffect +
+					activeEffect,
 				outline:
 					"border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 cursor-pointer transition-all duration-200",
 				secondary:
-					"bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary-darker hover:text-white cursor-pointer transition-all duration-200",
+					"bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary-darker hover:text-white cursor-pointer transition-all duration-200" +
+					hoverEffect +
+					activeEffect,
 				ghost:
 					"hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50 cursor-pointer",
 				link: "text-primary underline-offset-4 hover:underline ",
